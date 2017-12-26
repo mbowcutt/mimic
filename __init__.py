be6bb8e9f3011cb81feaf82a87bde196d89d7474
Michael Bowcutt <mwb71@case.edu>
2017-12-26 16:26:12 -0500

    added file upload

--- __init__.py
+++ __init__.py
@@ -1,22 +1,60 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+# Markov chain text generator
 import markovify
+
+UPLOAD_FOLDER='/tmp'
+ALLOWED_EXTENSIONS = set(['txt'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH']=16*1024*1024 # 16 Mb upload limit
 
-@app.route("/")
-def mimicme():
-    return "OK"
+@app.route("/", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            model = markovize(filename)
+            # return model.make_sentence()
 
-@app.route("/read")
-def read():
-    return "read"
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form
+    '''
+
+def markovize(filename):
+    with open(app.config['UPLOAD_FOLDER'] + "/" + filename) as f:
+        text=f.read()
+        return markovify.Text(text)
 
 @app.route("/speak")
 def speak():
-    return "spoke"
+    return model.make_sentence()
 
-@app.route("/upload")
-def upload():
-    return "upload"
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
     app.run()
