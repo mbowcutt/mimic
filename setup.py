4876c53ee7f4dd3eb2b81bba2298ef493452a80a
Michael Bowcutt <mwb71@case.edu>
2018-01-09 22:48:13 -0500

    init setuptoops

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Mimic',
+    version='1.0',
+    packages=['mimic'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+        'Flask-Migrate',
+        'Flask-WTF',
+        'Flask-Uploads',
+        'markovify'
+    ]
+)
