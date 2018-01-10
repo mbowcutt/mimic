3ee1a3a8c50d9729cfee326765ce019b400fbcae
Michael Bowcutt <mwb71@case.edu>
2018-01-09 23:07:42 -0500

    logging file dest

--- mimic/__init__.py
+++ mimic/__init__.py
@@ -7,7 +7,7 @@ import logging
 # Initialize app
 app = Flask(__name__)
 app.secret_key=os.urandom(24)
-logging.basicConfig(filename='app.log',level=logging.INFO)
+logging.basicConfig(filename='mimic/app.log',level=logging.INFO)
 
 from mimic.data import *
 from mimic.forms import *
